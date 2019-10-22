3ca438016455827fdf6fe840a5ccf9ad0f93cdfa
Paul O <paul.orawiec@gmail.com>
2019-10-22 07:06:14 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,12 @@
 # Add your variables here
+first_number = 5.to_f
+
+second_number = 3.to_f
+
+sum = first_number + second_number
+
+difference = first_number - second_number
+
+product = first_number * second_number
+
+quotient = first_number / second_number
