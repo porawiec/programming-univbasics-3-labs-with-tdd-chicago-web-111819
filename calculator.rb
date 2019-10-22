908d714a95f3eea690505f1bc4f1ec1bc25a4546
Paul O <paul.orawiec@gmail.com>
2019-10-22 06:59:28 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,32 @@
 # Add your variables here
+first_number = gets.to_f
+
+=begin
+def second_number(num2)
+  second_number = num2.to_f
+end
+
+def addition(first_number, second_number)
+  sum = first_number + second_number
+end
+
+def subtraction(first_number, second_number)
+  difference = first_number - second_number
+end
+
+def multiplication(first_number, second_number)
+  product = first_number * second_number
+end
+
+def division(first_number, second_number)
+  quotient = first_number / second_number
+end
+
+first_number
+second_number
+addition
+subtraction
+multiplication
+division
+
+=end
